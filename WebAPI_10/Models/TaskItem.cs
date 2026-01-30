25fd68489e67dc7af22526caab9d2285142fd0ca
Asif <asifali8562919531@gmail.com>
2026-01-30 15:05:16 +0530

    Commit

---
+++ WebAPI_10/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI_10.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public int Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ WebAPI_10/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI_10.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        // Navigation property
+        //public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
